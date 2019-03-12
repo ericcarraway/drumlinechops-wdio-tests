411b6fa86ef35a7295e8f9c8562fb57f6bb04328
Eric Lee Carraway <github@ericcarraway.com>
2019-03-12 11:41:06 -0500

    first minimum viable tests: privacy page

--- privacy.spec.js
+++ privacy.spec.js
@@ -1,1 +1,24 @@
 // https://drumlinechops.com/privacy
+const PrivacyPage = require(`./page-objects/privacy.page`);
+const page = new PrivacyPage();
+
+describe(`Privacy Page`, function () {
+  before(function () {
+    browser.url(page.url);
+  });
+
+  it(`should have the correct <h1> text`, function () {
+    const expected = `Privacy`
+    expect(page.h1.getText()).to.eql(expected);
+  });
+
+  describe(`text in the main content container`, function () {
+    it(`should include the phrase "personal information"`, function () {
+      expect(page.mainContentContainer.getText()).to.include(`personal information`);
+    });
+
+    it(`should include the phrase "subsidiaries or affiliates"`, function () {
+      expect(page.mainContentContainer.getText()).to.include(`subsidiaries or affiliates`);
+    });
+  });
+});
