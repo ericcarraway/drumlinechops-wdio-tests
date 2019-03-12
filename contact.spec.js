d397262d0a6e98ca1518b8625bc4a5781ae0a1fc
Eric Lee Carraway <github@ericcarraway.com>
2019-03-12 11:26:42 -0500

    first minimum viable tests: contact page

--- contact.spec.js
+++ contact.spec.js
@@ -1,1 +1,19 @@
 // https://drumlinechops.com/contact
+const ContactPage = require(`./page-objects/contact.page`);
+const page = new ContactPage();
+
+describe(`Contact Page`, function () {
+  before(function () {
+    browser.url(page.url);
+  });
+
+  it(`should have the correct <h1> text`, function () {
+    const expected = `Contact`
+    expect(page.h1.getText()).to.eql(expected);
+  });
+
+  it(`should have the correct submit button text`, function () {
+    const expected = `Send Message`
+    expect(page.btnSubmit.getText()).to.eql(expected);
+  });
+});
