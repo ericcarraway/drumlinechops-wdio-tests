51d49f794256dc39226702b57586d89f464a6bb3
Eric Lee Carraway <github@ericcarraway.com>
2019-03-14 10:34:04 -0500

    header - test "href" attribute for eight header links

--- header.spec.js
+++ header.spec.js
@@ -40,10 +40,59 @@ describe(`header`, () => {
   });
 
   // logo
-  describe(`link 1 of 8`, () => {
-    it(`should link to homepage`, () => {
+  describe(`link 1 of 8 - logo`, () => {
+    it(`should link to the homepage`, () => {
       const expected = `https://drumlinechops.com/`;
       expect(header.links[0].getAttribute(`href`)).to.eql(expected);
+    });
+  });
+
+  describe(`link 2 of 8 - "Home"`, () => {
+    it(`should link to the homepage`, () => {
+      const expected = `https://drumlinechops.com/`;
+      expect(header.links[1].getAttribute(`href`)).to.eql(expected);
+    });
+  });
+
+  describe(`link 3 of 8 - "About"`, () => {
+    it(`should link to "/about"`, () => {
+      const expected = `https://drumlinechops.com/about`;
+      expect(header.links[2].getAttribute(`href`)).to.eql(expected);
+    });
+  });
+
+  describe(`link 4 of 8 - "Lessons"`, () => {
+    it(`should link to "/lessons"`, () => {
+      const expected = `https://drumlinechops.com/lessons`;
+      expect(header.links[3].getAttribute(`href`)).to.eql(expected);
+    });
+  });
+
+  describe(`link 5 of 8 - "Practice"`, () => {
+    it(`should link to "/practice_routine"`, () => {
+      const expected = `https://drumlinechops.com/practice_routine`;
+      expect(header.links[4].getAttribute(`href`)).to.eql(expected);
+    });
+  });
+
+  describe(`link 6 of 8 - "Blog"`, () => {
+    it(`should link to "/posts"`, () => {
+      const expected = `https://drumlinechops.com/posts`;
+      expect(header.links[5].getAttribute(`href`)).to.eql(expected);
+    });
+  });
+
+  describe(`link 7 of 8 - "Signup" button`, () => {
+    it(`should link to "users/sign_up"`, () => {
+      const expected = `https://drumlinechops.com/users/sign_up`;
+      expect(header.links[6].getAttribute(`href`)).to.eql(expected);
+    });
+  });
+
+  describe(`link 8 of 8 - "Login" button`, () => {
+    it(`should link to "users/sign_in"`, () => {
+      const expected = `https://drumlinechops.com/users/sign_in`;
+      expect(header.links[7].getAttribute(`href`)).to.eql(expected);
     });
   });
 });
