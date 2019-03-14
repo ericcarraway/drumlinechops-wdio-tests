e8ecc8a642ca187ee8805b57ebf134c5dae4af1e
Eric Lee Carraway <github@ericcarraway.com>
2019-03-14 11:15:37 -0500

    footer: scaffold tests for three elements
    navigation links
    social links
    copyright

--- footer.spec.js
+++ footer.spec.js
@@ -20,4 +20,23 @@ describe(`footer`, () => {
       expect(footerText).to.include(`© 2012-2019 Drumline Chops`);
     });
   });
+
+  describe(`navigation links`, () => {
+    it(`should have five links`, () => {
+      expect(footer.navigationLinks.length).to.eql(5);
+    });
+  });
+
+  describe(`social links`, () => {
+    it(`should have three links`, () => {
+      expect(footer.socialLinks.length).to.eql(3);
+    });
+  });
+
+  describe(`copyright`, () => {
+    it(`should the correct text`, () => {
+      const expected = `© 2012-2019 Drumline Chops`;
+      expect(footer.copyright.getText()).to.eql(expected);
+    });
+  });
 });
--- page-objects/footer.page.js
+++ page-objects/footer.page.js
@@ -2,6 +2,18 @@ class Footer {
   get wrapper() {
     return $(`footer`);
   }
+
+  get navigationLinks() {
+    return $$(`.footer-menu ul:first-child li`);
+  }
+
+  get socialLinks() {
+    return $$(`footer ul#socials li`);
+  }
+
+  get copyright() {
+    return $(`footer p`);
+  }
 }
 
 module.exports = Footer;
