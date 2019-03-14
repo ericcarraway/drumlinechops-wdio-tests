0b6c9c41ba43ee5a559bf98eab335fefd8fe9bf9
Eric Lee Carraway <github@ericcarraway.com>
2019-03-14 09:37:16 -0500

    scaffold tests for header and footer

---
+++ header.spec.js
@@ -0,0 +1,25 @@
+// https: //drumlinechops.com/
+
+const Header = require(`./page-objects/header.page`);
+const header = new Header();
+
+describe(`header`, () => {
+  before(() => {
+    browser.url(`/`);
+  });
+
+  describe(`header text`, () => {
+    it(`should include key words`, () => {
+      const headerText = header.navbar.getText();
+
+      expect(headerText).to.include(`Home`);
+      expect(headerText).to.include(`About`);
+      expect(headerText).to.include(`Lessons`);
+      expect(headerText).to.include(`Practice`);
+      expect(headerText).to.include(`Blog`);
+
+      expect(headerText).to.include(`Signup`);
+      expect(headerText).to.include(`Login`);
+    });
+  });
+});
